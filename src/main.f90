b9d57c34dfb1a357610ec99d25e2c298b4ddd96d
Michal <michal@owsiak.org>
2022-06-08 09:25:25 +0200

    Update main.f90

--- src/main.f90
+++ src/main.f90
@@ -6,7 +6,7 @@ program main
   type(ids_summary)   :: summary
   integer             :: status_read, status_write
   integer             :: pctx_read,   pctx_write
-  
+
   character(len=132)  :: usr
 
   integer             :: shot_read,  run_read
@@ -24,13 +24,15 @@ program main
 
   call get_environment_variable("USER",usr)
 
+  write (*,*) 'Reading input data'
+
 ! Here is the part responsible for opening/creating pulse files
   call ual_begin_pulse_action(MDSPLUS_BACKEND, shot_read, run_read, usr, "test", "3", pctx_read, status_read)
   if (status_read.ne.0) ERROR STOP 'Error beggining the pulse context - input'
- 
+
   call ual_open_pulse(pctx_read, OPEN_PULSE, "", status_read)
   if (status_read.ne.0) ERROR STOP 'Error opening the pulse file - input'
-  
+
   call ual_begin_pulse_action(MDSPLUS_BACKEND, shot_write, run_write, usr, "test", "3", pctx_write, status_write)
   if (status_write.ne.0) ERROR STOP 'Error beggining the pulse context - output'
 
@@ -40,10 +42,13 @@ program main
 ! This is the business logic of the code
   call ids_get(pctx_read, "summary", summary)
 
+  write (*,*) 'summary%ids_properties%comment: ', summary%ids_properties%comment
+
   summary%ids_properties%comment = 'Modified by the code'
 
   call ids_put(pctx_write, "summary", summary)
 
+  write (*,*) 'Writing output data'
 
 ! Here is the part responsible for closing pulse files
   call ual_close_pulse(pctx_read, CLOSE_PULSE, "", status_read)
